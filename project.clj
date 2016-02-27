60c985da46910c377dca9940aa76f8c4e35a31f8
Shane Kilkelly <shane@kilkelly.me>
2016-02-27 16:24:18 +0000

    deps

--- project.clj
+++ project.clj
@@ -5,15 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.postgresql/postgresql "9.4-1206-jdbc4"]
                  ]
-  :profiles
-  {:dev {:dependencies [
-                        [org.clojure/java.jdbc "0.4.2"]
-                        [org.postgresql/postgresql "9.4-1206-jdbc4"]
-                        ]}
-   :test {:dependencies [
-                         [org.clojure/java.jdbc "0.4.2"]
-                         [org.postgresql/postgresql "9.4-1206-jdbc4"]
-                         ]}
-
    })
