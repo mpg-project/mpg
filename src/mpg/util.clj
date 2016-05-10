c0f630c335a090f3396b1250d6091f5c286cb920
James Laver <james.laver@gmail.com>
2016-05-10 09:31:33 +0000

    More debugging for pg-param-type

--- src/mpg/util.clj
+++ src/mpg/util.clj
@@ -5,7 +5,10 @@
 
 (defn pg-param-type
   [^PreparedStatement s ^long idx]
-  (-> s .getMetaData (.getColumnTypeName idx)))
+  (if-let [md (.getMetaData s)]
+    (or (.getColumnTypeName md idx)
+        (throw (ex-info "We could not obtain the column type name" {:got s :meta md})))
+    (throw (ex-info "We could not obtain metadata from the prepared statement" {:got s}))))
 
 (defn pg-json
   "Converts the given value to a PG JSON object"
